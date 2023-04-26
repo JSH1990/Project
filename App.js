4f0e503ff2f090352ddb5b0e79711fba46da76a0
jshblack <118979741+JSH1990@users.noreply.github.com>
2023-04-26 15:19:43 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Login from './signup/Login';
+import Home from './pages/Home';
+import Signup from './signup/Signup';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
